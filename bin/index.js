e0ae8a005b82603e6d4e33774aa9a956411a7bc8
Charlie Lee <dev@charlielee.uk>
2020-05-03 21:40:49 +0100

    Add a webrtc  test

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 (function() {
   "use strict";
   // Create a new window and get it
-  nw.Window.open('https://github.com', {}, function(new_win) {
+  nw.Window.open('bin/index.html', {}, function(new_win) {
     // And listen to new window's focus event
     new_win.on('focus', function() {
       console.log('New window is focused');
